f27ce1612a70b9773ba477c9920c492a3ac3cf4d
HeZaichang.WeRDyin <heyin223@163.com>
2018-08-29 20:31:20 +0800

    Add README

---
+++ README.md
@@ -0,0 +1,10 @@
+<p align="center">
+  <img src="https://user-images.githubusercontent.com/10026019/44260701-d84b6e80-a247-11e8-9d79-5f82be615c84.png" width="300"/>
+  <b>tracker</b>
+</p>
+
+<p align="center">
+	<a href="https://www.npmjs.com/package/@anka-dev/tracker">
+		<img src="https://badge.fury.io/js/%40anka-dev%2Ftracker.svg"/>
+	</a>
+</p>
--- build/rollup.config.prod.js
+++ build/rollup.config.prod.js
@@ -1,12 +1,17 @@
 import uglify from 'rollup-plugin-uglify-es'
 import banner from 'rollup-plugin-license'
 import rollupConfig from './rollup.config.base'
+import { version, license, homepage } from '../package.json'
 
 export default Object.assign(rollupConfig, {
     plugins: rollupConfig.plugins.concat([
         uglify(),
         banner({
-            banner: `@anka-dev/tracker. \n${ new Date() } \n MIT License.`
+            banner: '@anka-dev/tracker.' + '\r\n' +
+             'V' + version + '\r\n' +
+             new Date() + '\r\n' +
+             homepage + '\r\n' +
+             license + ' License'
         })
     ]),
     output: Object.assign(rollupConfig.output, {
